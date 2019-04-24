826d63ad1074cc065624b44c8891d63de6ccc6cc
Elisa <you@example.com>
2019-04-24 15:19:21 +0200

    Vec2 Aufgabe 2.4

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -20,3 +20,25 @@ Vec2& Vec2::operator *=(float s){
     y = y / s;
     return *this;
  }
+ Vec2 operator + (Vec2 const & u, Vec2 const & v){
+    Vec2 w = u;
+    return w += v;
+ }
+Vec2 operator - (Vec2 const & u, Vec2 const & v){
+    Vec2 w = u;
+    return w -= v;
+}
+Vec2 operator * (Vec2 const & v, float s){
+    Vec2 w = v;
+    return w *= s;
+}
+Vec2 operator / (Vec2 const & v, float s){
+    Vec2 w = v;
+    return w /= s;
+}
+Vec2 operator * (float s, Vec2 const & v){
+    Vec2 w = v;
+    w.x = s * w.x;
+    w.y = s * w.y;
+    return w; 
+}
